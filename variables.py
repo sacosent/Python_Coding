bb59ec0dde0a4b81ed8e3716dcfc16bcbb8f7603
Santiago Cosentino <72815091+sacosent@users.noreply.github.com>
2024-05-15 13:46:57 -0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Variables in Python
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
