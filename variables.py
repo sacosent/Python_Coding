c8b59e034928f4c5a14f591fbd3409a58fcf744b
Santiago Cosentino <cosentinosantiago92@gmail.com>
2024-05-17 13:44:20 -0300

    Print of variables

--- variables.py
+++ variables.py
@@ -12,3 +12,17 @@ person_info = {
    'country':'Finland',
    'city':'Helsinki'
    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
